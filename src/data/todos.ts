b13bb1bb1ba559be799acb170f02a10fbf2bfe4f
Goat3ggs <stefpandi12@gmail.com>
2025-05-05 21:37:40 +0300

    final commit

---
+++ src/data/todos.ts
@@ -0,0 +1,19 @@
+import { Todo } from "../types/todo";
+
+export const dummyData: Todo[] = [
+    {
+        id: 1,
+        title: "First dummy text.",
+        completed: false,
+    },
+    {
+        id: 2,
+        title: "Second dummy text.",
+        completed: false,
+    },
+    {
+        id: 3,
+        title: "Third dummy text.",
+        completed: false,
+    },
+];
