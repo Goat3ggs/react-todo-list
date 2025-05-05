18fc30a9a95bf7ff43c318fc5e2b5d9e0b127f84
Goat3ggs <stefpandi12@gmail.com>
2025-05-05 22:06:21 +0300

    another one

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/react-todo-list/', // <- asta e cheia pentru GitHub Pages
   plugins: [
     react(),
     tailwindcss(),
