168dccb459ec6a806d0d8f1a8e668c973f8f9049
Goat3ggs <stefpandi12@gmail.com>
2025-07-06 22:55:13 +0300

    delete test objects

--- src/data/todos.ts
+++ src/data/todos.ts
@@ -1,19 +1,3 @@
 import { Todo } from "../types/todo";
 
-export const dummyData: Todo[] = [
-    {
-        id: 1,
-        title: "First dummy text.",
-        completed: false,
-    },
-    {
-        id: 2,
-        title: "Second dummy text.",
-        completed: false,
-    },
-    {
-        id: 3,
-        title: "Third dummy text.",
-        completed: false,
-    },
-];
+export const dummyData: Todo[] = [];
